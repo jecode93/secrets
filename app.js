3439b39c1df9451982a0087646fcef73c329c8b5
jecode93 <jeanemmanuelcadet@gmail.com>
2023-01-18 03:48:46 -0500

    Installation of some packages that I want like, express, body-parser and ejs.

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 //jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const { appendFile } = require("fs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view-engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+
+
+app.listen(3000, function () {
+    console.log("Server listen on port 3000");
+})
