0563506a036a1799d4c0cfc07fdf8437a68fc8b6
jecode93 <jeanemmanuelcadet@gmail.com>
2023-01-23 06:40:08 -0500

    Create route for the secret app and add the 'ejs' view engine.

--- app.js
+++ app.js
@@ -7,13 +7,21 @@ const { appendFile } = require("fs");
 const app = express();
 
 app.use(express.static("public"));
-app.set("view-engine", "ejs");
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+app.get("/", function (req, res) {
+    res.render("home");
+});
 
+app.get("/login", function (req, res) {
+    res.render("login");
+});
 
-
+app.get("/register", function (req, res) {
+    res.render("register");
+});
 
 
 app.listen(3000, function () {
