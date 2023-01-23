c0b14c946a1d21085ffaa8869f527534e20ee6f3
jecode93 <jeanemmanuelcadet@gmail.com>
2023-01-23 07:09:00 -0500

    Create database, and connect the database using mongoose.

--- app.js
+++ app.js
@@ -3,12 +3,36 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { appendFile } = require("fs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//DB Name
+const dbname = "userDB";
+
+//DB url
+const url = "mongodb://0.0.0.0:27017/";
+
+//DB connections and the database creation
+mongoose.set("strictQuery", false);
+mongoose.connect(url + dbname, { useNewUrlParser: true });
+
+//Creation of the items Schema
+const userSchema = mongoose.Schema({
+    email: String,
+    password: String
+});
+
+//Use the Schema to create the collection model of the database with the singular name.
+const User = mongoose.model("User", userSchema);
+
+
+
 
 
 app.get("/", function (req, res) {
